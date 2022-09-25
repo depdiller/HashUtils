cb336471057a3246e5ea78ac9b799276e8eecd5b
depdiller <stanislavoronov@yandex.ru>
2022-09-25 21:23:48 +0300

    Add README

---
+++ README.md
@@ -0,0 +1,64 @@
+## Лабораторная работа №1
+
+### Описание утилит:
+- Gen
+```
+usage: gen.py [-h] [-e ENCODING] [-f FUNCTION] [-o OUTPUT] file n
+
+Hash generator
+
+positional arguments:
+  file                  Text file with passwords
+  n                     How many hash codes should be generated
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -e ENCODING, --encoding ENCODING
+                        Choose encoding
+  -f FUNCTION, --function FUNCTION
+                        Choose which hash functions to use
+  -o OUTPUT, --output OUTPUT
+                        Output file to store hash codes
+```
+
+- Crack
+```
+usage: crack.py [-h] [-e ENCODING] [-f FUNCTION] dict_file hash_file
+
+Crack passwords
+
+positional arguments:
+  dict_file             List of word - password candidates
+  hash_file             List of hashes
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -e ENCODING, --encoding ENCODING
+                        Choose encoding
+  -f FUNCTION, --function FUNCTION
+                        Choose which hash functions to use
+```
+
+##### Поддерживаемые кодировки и хэш-функции:
+###### Кодировки:
+- utf-8
+- ascii
+- utf-16-be
+- utf-16-le
+###### Хэш-функции
+- md4
+- md5
+- sha256
+- sha1
+- sha512
+
+Стандартные параметры: кодировка - utf-8, хэш-функция - sha256, выходной файл с хэшами в утилите `gen` - output.txt
+
+### Benchmark
+
+Параметры среды: 
+- Процессор: *Apple M1 8 ядер*
+- Размер словаря кондидатов в пароли: *492_086 слов*
+- Размер словаря хэшей: *50_000 слов*
+
+Время работы утилиты `crack` - 249 секунд, что составляет около *1_976 кондидатов/секунду*
--- crack.py
+++ crack.py
@@ -1,15 +1,17 @@
-import re
-from this import d
+from multiprocessing.dummy import Pool
 from parameters import Encoding
 from parameters import HashFunctions
 import argparse
 import multiprocessing
+import time
+
+THREADS = multiprocessing.cpu_count()
 
 parser = argparse.ArgumentParser(description='Crack passwords')
-parser.add_argument('hashlist', type=str, metavar='file',
-    help='List of hashes')
-parser.add_argument('dictionary', type=str, metavar='file',
+parser.add_argument('dictionary', type=str, metavar='dict_file',
     help='List of word - password candidates')
+parser.add_argument('hashlist', type=str, metavar='hash_file',
+    help='List of hashes')
 parser.add_argument('-e', '--encoding', type=Encoding,
     help='Choose encoding', default=Encoding.utf8)
 parser.add_argument('-f', '--function', type=HashFunctions,
@@ -22,35 +24,53 @@ encoding = args.encoding.value
 hash_function_name = args.function
 
 def compute_hash(line):
+    line = line.rstrip('\n')
     hash_object = hash_function_name.get_hash_object()
     hash_object.update(line.encode(encoding=encoding))
-    return hash_object.hexdigest()
+    hash = hash_object.hexdigest()
+    return hash
 
-result_dictionary = None
-
-def worker(hash, word_list):
-    matches = [word for word in word_list if compute_hash(word) == hash]
-    result_dictionary[hash] = matches
+def worker(hashed_passwords, chunk_of_dict):
+    local_dict = dict()
+    for word in chunk_of_dict:
+        hashed_word = compute_hash(word)
+        matches = [hash for hash in hashed_passwords if hashed_word == hash]
+        if (len(matches) != 0):
+            local_dict[word] = matches
+    return local_dict
     
-word_list = None
+hashed_passwords = None
 
-def worker_wrapper(hash):
-    return worker(word_list, hash)
+def worker_wrapper(chunk_of_dict):
+    return worker(hashed_passwords, chunk_of_dict)
 
-def worker_init(dictionary):
-    global word_list
-    global result_dictionary
-    word_list = dictionary
-    result_dictionary = dict()
-
-with open(hashlist_path, 'r') as hashlist, open(dictionary_path, 'r') as dictionary:
-    init_dict = dictionary.readlines()
-    pool = multiprocessing.Pool(proccesses=4, initializer=worker_init, initargs=(init_dict))
-    init_hashes = hashlist.readlines()
-    pool.map(worker_wrapper, init_hashes)
-    pool.close()
-    pool.join()
-    pool.terminate()
+def worker_init(hashes):
+    global hashed_passwords
+    hashed_passwords = hashes
     
+def main():
+    answer = []
+    with open(hashlist_path, 'r') as hashlist, open(dictionary_path, 'r') as dictionary:
+        dict = dictionary.readlines()
+        dict = [el.rstrip('\n') for el in dict]
+        init_hashes = hashlist.readlines()
+        init_hashes = [hash.rstrip('\n') for hash in init_hashes]
+        chunksize = int(len(init_hashes) / THREADS)
+        chunksize = chunksize if chunksize > 0 else chunksize + 1
+        chunks = [dict[i: i + chunksize]
+            for i in range(0, len(dict), chunksize)]
+        pool = multiprocessing.Pool(THREADS, initializer=worker_init, initargs=(init_hashes,))
+        start_time = time.perf_counter()
+        for chunk in chunks:
+            result_dict = pool.apply_async(worker_wrapper, (chunk,))
+            answer.append(result_dict)
+        pool.close()
+        pool.join()
+    for async_res in answer:
+        dict = async_res.get()
+        if dict:
+            print(dict)
+    print(f"Completed Execution in {time.perf_counter() - start_time} seconds")
 
-print(result_dictionary)
+if __name__ == '__main__':
+    main()
